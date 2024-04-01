5914efa17a5e2bb416de24fe87badeb3c0edacb3
Luke Rokop <rokopluk@msu.edu>
2024-04-01 13:23:57 -0400

    completed app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,72 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+  width: '264',
+  height: '378'
+};
+
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+];
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+      }
+    </div>
+  );
+}
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'red' : 'green'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+
+function MagicButton() {
+  const [count, setCount] = useState(0);
+  function doMagic() {
+    setCount(count + 1);
+    alert('Are you not entertained?');
+  }
+
+  return (
+    <button onClick={doMagic}>
+      Magic {count} times
+    </button>
+  );
+}
 
 function App() {
   return (
@@ -7,7 +74,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload wooooooo it works.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +82,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <div>
+          <MagicButton />
+          <ZineRack />
+          <Bookshelf />
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
